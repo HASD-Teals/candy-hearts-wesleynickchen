1e38f613ecd64c0e01f6d44d84d984d8f8050951
wesleynickchen <wesley.nickchen@gmail.com>
2022-01-24 10:04:49 -0600

    detroit urban survival training

--- src/HeartsClient.java
+++ src/HeartsClient.java
@@ -8,5 +8,6 @@ public class HeartsClient {
         Heart Group1 = new Heart("red", 3, "Hi", 5, 5.00, true, 50);
         Heart Group2 = new Heart("teal", 5, "Love CS-A", 5, 4.50, true, 200);
         Heart Group3 = new Heart("green", 2, "Go Girl!", 5, 3.50, true, 75);
+    
     }
 }
